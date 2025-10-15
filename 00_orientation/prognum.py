b01f6d4169ed3e7d4a5c5d728d01f67b4d2efbaa
chikara <chikara0422@gmail.com>
2025-10-15 19:26:40 +0900

    再帰的な関数を利用してフィボナッチ数列を実装しようの課題完了

--- 00_orientation/prognum.py
+++ 00_orientation/prognum.py
@@ -1,9 +1,18 @@
 # prognum.py
 
-def fibonacci(n: int) -> int:
-    """n番目のフィボナッチ数を返す（1始まり）"""
-    if n == 1 or n == 2:   # 1番目と2番目は1
-        return 1
-    else:                  # 3番目以降は直前2つの和
-        return fibonacci(n - 1) + fibonacci(n - 2)
+def fibonacci(n):
+    """
+    再帰的にフィボナッチ数列の n 番目の値を求める関数
     
+    ルール：
+    1番目の値 → 1
+    2番目の値 → 1
+    3番目以降 → 直前2つの値の和
+    """
+    if n <= 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        # 再帰呼び出し：自分自身を使って計算
+        return fibonacci(n - 1) + fibonacci(n - 2)
